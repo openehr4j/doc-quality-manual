183456cc9e923fbbc144b09d690c239ecdc3d043
Jan Mewes <jan.mewes@ks-plus.org>
2024-06-21 07:13:02 +0200

    Publish to gh pages

--- publish.sh
+++ publish.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-TAG_NAME=0.0.1
-
+VERSION=0.0.1
 RELEASE_ARTIFACT_DIR=$(mktemp -d)
+TODAY=$(date '+%Y-%m-%d')
 
-# Compile HTML
+echo "##########################################################"
+echo "Compile HTML"
+echo "##########################################################"
 if [[ -d ./build ]]; then
   rm -r ./build
 fi
@@ -13,15 +15,24 @@ find src -name "img" -exec cp -r {} ./build \;
 docker run -it -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor ./src/index.adoc --out-file ./build/quality-manual.html
 mv ./build ${RELEASE_ARTIFACT_DIR}/html
 
-# Compile PDF
+echo "##########################################################"
+echo "Compile PDF"
+echo "##########################################################"
 mkdir ./build
 find src -name "img" -exec cp -r {} ./src \;
 docker run -it -v $(pwd):/documents/ asciidoctor/docker-asciidoctor asciidoctor-pdf ./src/index.adoc --out-file ./build/quality-manual.pdf
-rm $(git ls-files --others --exclude-standard) # remove untracked files
+rm $(git ls-files --others --exclude-standard) && rmdir ./src/img 2> /dev/null # remove untracked files
 mkdir ${RELEASE_ARTIFACT_DIR}/pdf
 mv ./build/quality-manual.pdf ${RELEASE_ARTIFACT_DIR}/pdf/
 
-find $RELEASE_ARTIFACT_DIR
-
-# GH_PAGES_DIR=$(mktemp -d)
-# gh repo clone openehr4j/quality-manual ${GH_PAGES_DIR} -- -b gh-pages --single-branch
+echo "##########################################################"
+echo "Upload to GitHub Pages"
+echo "##########################################################"
+GH_PAGES_DIR=$(mktemp -d)
+gh repo clone openehr4j/quality-manual ${GH_PAGES_DIR} -- -b gh-pages --single-branch
+cd ${GH_PAGES_DIR}
+mv ${RELEASE_ARTIFACT_DIR} ${GH_PAGES_DIR}/${VERSION}
+echo "| ${VERSION} | ${TODAY} | [quality-manual.html](./${VERSION}/html/quality-manual.html) | [quality-manual.pdf](./${VERSION}/pdf/quality-manual.pdf) |" >> index.md
+git add .
+git commit -m "Add release artifacts for version ${VERSION}"
+git push origin gh-pages
